0cbf1dd5af08bd90986074da693f7139698c692e
Silverkike <enrique_guardado@outlook.es>
2026-07-23 18:40:56 -0600

    fix: remove pnpm-lock and fix middleware for vercel

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
 
-// Forzar el uso de Node.js Runtime en lugar de Edge Runtime
-export const runtime = 'nodejs';
-
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
