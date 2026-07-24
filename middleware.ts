6c321e6eec9262c55a40a3ad9cd5f776b487960e
Silverkike <enrique_guardado@outlook.es>
2026-07-23 18:24:05 -0600

    fix: force nodejs runtime in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
 
+// Forzar el uso de Node.js Runtime en lugar de Edge Runtime
+export const runtime = 'nodejs';
+
 export const config = {
-    // https://nextjs.org/docs/app/api-reference/file-conventions/middleware#matcher
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
